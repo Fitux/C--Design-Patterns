846e476b6b80be1c8ae16e2797937d2f941e0953
Luis Felipe Sanchez <imfitux@outlook.com>
2018-01-26 19:41:35 -0600

    Factory Excercise
    You are given a class called Person . The person has two fields: Id , and Name .
    
    Please implement a non-static PersonFactory that has a CreatePerson() method that takes a person's name.
    
    The Id of the person should be set as a 0-based index of the object created. So, the first person the factory makes should have Id=0, second Id=1 and so on.

---
+++ Factory Coding Excercise/Factory Coding Excercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Coding_Excercise
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonFactory
+    {
+        private int index = 0;
+
+        public Person CreatePerson(string name)
+        {
+            var person = new Person();
+            person.Id = index;
+            person.Name = name;
+
+            index++;
+
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
