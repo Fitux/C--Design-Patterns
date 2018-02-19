cc630f9be70bac2d26ca9ba05cc83cf7576755db
Luis Felipe Sanchez <imfitux@outlook.com>
2018-02-19 16:56:10 -0600

    Excercise of Prototype
    Not using Serialization

---
+++ Prototype Excercise/Prototype Excercise/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Excercise
+{
+    namespace Coding.Exercise
+    {
+        public class Point
+        {
+            public int X, Y;
+
+            public Point (int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+
+            public Point Clone()
+            {
+                return new Point(this.X, this.Y);
+            }
+        }
+
+        public class Line
+        {
+            public Point Start, End;
+
+            public Line (Point Start, Point End)
+            {
+                if (this.Start == null)
+                    throw new ArgumentNullException("Argument Null");
+
+                if (this.End == null)
+                    throw new ArgumentNullException("Argument Null");
+
+                this.Start = Start;
+                this.End = End;
+            }
+
+            public Line DeepCopy()
+            {
+                return new Line(this.Start.Clone(), this.End.Clone());
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
